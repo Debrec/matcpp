ba74f5f01587e029ef5f91c3e9c2853bf177efea
Hernán Braile <hbraile@gmail.com>
2018-06-04 11:21:04 -0300

    Se borra archivo que no se usa mas

--- conectar.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "hernan", "test", "comentarios");
-if ($mysqli === false) {
-  die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-$mysqli->set_charset('utf8');
-?>
