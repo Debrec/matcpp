b09abc9c3c639f203120156550d7c639be77291b
Hernán Braile <hgbraile@outlook.com.ar>
2020-10-09 11:47:45 -0300

    Se agregan titulos en algoritmos

--- aglateral.php
+++ aglateral.php
@@ -1,8 +1,16 @@
 <?php
+$title=true;
 foreach ($alglinks as $clave => $valor) {
 	if (is_int($clave)) {
+        if ($clave == 0) {
+           echo "<h2 class=subt>Descargas</h2>";
+        }
 		echo "<p><a class=texto href=\"$valor[1]\">" . $valor[0] . '</a></p>';
 	} else {
+        if ($title) {
+           echo "<h2 class=subt>Paginas</h2>";
+            $title = false;        
+        }
 		echo '<p><a class=texto href="./index.php?pag=' . $clave . '&sub=alg">' . $valor[0] . '</a></p>';
 	}
 }
--- algorithm.php
+++ algorithm.php
@@ -3,6 +3,9 @@
 <?php
 foreach ($alglinks as $clave => $valor) {
 	if (is_int($clave)) {
+        if ($clave == 0) {
+           echo "<h1 class=subt>Descargas</h1>";
+        }
 		echo "<p><a class=texto href=\"$valor[1]\">" . $valor[0] . '</a><span class=p>' . $valor[2] . '</span></p>';
 	} else {
 		echo '<p><a class=texto href="./index.php?pag=' . $clave . '&sub=alg">' . $valor[0] . '</a><span class=p>' . $valor[2] . '</span></p>';
